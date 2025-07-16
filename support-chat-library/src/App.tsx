20cbacce8e700f1299bcfeeed5d093bcdabb2f45
Maury SO <lynuxazura@gmail.com>
2025-07-16 16:49:35 -0600

    Init Proyect

---
+++ support-chat-library/src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <h1
+        className="text-4xl"
+      >
+        Hola
+      </h1>
+      <Button variant={"default"}>Click me</Button>
+    </>
+  )
+}
+
+export default App
